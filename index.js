b06e4babea4437d30eb687cd65a4431d1366e32f
Jonny <jonny.howle@gmail.com>
2022-05-28 13:20:41 -0600

    commit tree ac647a6f1c53d3ae6b81cf1ea23c40b53e6fc2b5

--- index.js
+++ index.js
@@ -1,9 +1,32 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  if (word.length % 2 == 0) {
+    for (let i = 0; i <= (word.length / 2) - .5; i++) {
+      if (word.charAt(i) === word.charAt(word.length - i - 1)){
+        return true
+      } else {
+        return false
+      }
+    }
+  } else {
+    for (let i = 0; i <= (word.length / 2) - .5; i++) {
+      if (word.charAt(i) === word.charAt(word.length - i - 1)){
+        return true
+      } else {
+        return false
+      }
+    }
+  }
+
 }
 
 /* 
-  Add your pseudocode here
+  The first thing the function should do is determine if the string is even or odd in length
+  To do this
+  If the string is even, get the first letter and the last letter and compare them
+  If the result is true, move to the second letter and the second to last letter and compare them
+  If true, continue this process until "i" is equal to half the string length
+  If the string is odd complete the same steps except quit the loop once "i" is equal to half the string length mine .5
 */
 
 /*
